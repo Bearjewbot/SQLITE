66a816f35d3b8cbd28c1e93d23ca147c77a95d96
Daniel Lovened <daniel_lovened@hotmail.com>
2025-02-15 19:39:06 +0100

    add project entity// add data context// install EFC packages

---
+++ Data/Entities/ProjectEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities;
+
+public class ProjectEntity
+{
+    [Key]
+    public int ProjectNumber { get; set; }
+
+    [Required]
+    [Column(TypeName = "nvarchar(100)")]
+    public string Description { get; set; } = null!;
+
+    [Column(TypeName = "nvarchar(max)")]
+    public string? Notes { get; set; }
+}
