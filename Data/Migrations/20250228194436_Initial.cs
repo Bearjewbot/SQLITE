582652c8886a3f3368053e82bbc4decad63c03ef
Daniel Lovened <daniel_lovened@hotmail.com>
2025-03-01 16:59:32 +0100

    fixes

---
+++ Data/Migrations/20250228194436_Initial.cs
@@ -0,0 +1,271 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomerTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CustomerType = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServiceTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ServiceType = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StatusTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StatusType = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StatusTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    ContactPerson = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true),
+                    CustomerTypeId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Customers_CustomerTypes_CustomerTypeId",
+                        column: x => x.CustomerTypeId,
+                        principalTable: "CustomerTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserContactInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Email = table.Column<string>(type: "TEXT", maxLength: 150, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "TEXT", maxLength: 17, nullable: true),
+                    UserEntityId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserContactInfo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserContactInfo_Users_UserEntityId",
+                        column: x => x.UserEntityId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Notes = table.Column<string>(type: "TEXT", maxLength: 500, nullable: true),
+                    Price = table.Column<int>(type: "INTEGER", nullable: false),
+                    StartDate = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    EndDate = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    CustomerEntityId = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserEntityId = table.Column<int>(type: "INTEGER", nullable: false),
+                    StatusTypeEntityId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ServiceTypeEntityId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Projects_Customers_CustomerEntityId",
+                        column: x => x.CustomerEntityId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Projects_ServiceTypes_ServiceTypeEntityId",
+                        column: x => x.ServiceTypeEntityId,
+                        principalTable: "ServiceTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Projects_StatusTypes_StatusTypeEntityId",
+                        column: x => x.StatusTypeEntityId,
+                        principalTable: "StatusTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Projects_Users_UserEntityId",
+                        column: x => x.UserEntityId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CustomerTypes",
+                columns: new[] { "Id", "CustomerType" },
+                values: new object[,]
+                {
+                    { 1, "Företag" },
+                    { 2, "Privat Person" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ServiceTypes",
+                columns: new[] { "Id", "ServiceType" },
+                values: new object[,]
+                {
+                    { 1, "Konsultation 1000kr/tim" },
+                    { 2, "Utveckling 1500kr/tim" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "StatusTypes",
+                columns: new[] { "Id", "StatusType" },
+                values: new object[,]
+                {
+                    { 1, "Planerad" },
+                    { 2, "Påbörjad" },
+                    { 3, "Avslutad" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "Lars", "Björnsson" },
+                    { 2, "Gunnar", "Strömming" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "Id", "ContactPerson", "CustomerTypeId", "Name" },
+                values: new object[,]
+                {
+                    { 1, null, 1, "Bobby AB" },
+                    { 2, null, 2, "Björn Gustavsson" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "UserContactInfo",
+                columns: new[] { "Id", "Email", "PhoneNumber", "UserEntityId" },
+                values: new object[,]
+                {
+                    { 1, "Doggy@hotmail.com", null, 1 },
+                    { 2, "Catty@hotmail.com", null, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Projects",
+                columns: new[] { "Id", "CustomerEntityId", "Description", "EndDate", "Notes", "Price", "ServiceTypeEntityId", "StartDate", "StatusTypeEntityId", "UserEntityId" },
+                values: new object[] { 101, 1, "Bästa projektet EU", "2026-02-25", null, 400000, 1, "2024-01-25", 1, 1 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_CustomerTypeId",
+                table: "Customers",
+                column: "CustomerTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_CustomerEntityId",
+                table: "Projects",
+                column: "CustomerEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_ServiceTypeEntityId",
+                table: "Projects",
+                column: "ServiceTypeEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_StatusTypeEntityId",
+                table: "Projects",
+                column: "StatusTypeEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_UserEntityId",
+                table: "Projects",
+                column: "UserEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserContactInfo_UserEntityId",
+                table: "UserContactInfo",
+                column: "UserEntityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "UserContactInfo");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "ServiceTypes");
+
+            migrationBuilder.DropTable(
+                name: "StatusTypes");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "CustomerTypes");
+        }
+    }
+}
